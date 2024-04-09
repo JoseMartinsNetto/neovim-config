98057165e1b40bd8e0b6528328ae47df33e25a01
José Martins <j.msantos.netto@gmail.com>
2024-04-09 16:09:24 -0300

    Some fixes

--- init.lua
+++ init.lua
@@ -185,6 +185,7 @@ require('lazy').setup({
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+        ['<leader>t'] = { name = '[T]ests', _ = 'which_key_ignore' },
       }
     end,
   },
--- lua/custom/ui/ui.lua
+++ lua/custom/ui/ui.lua
@@ -61,38 +61,5 @@ return {
       }
     end,
   },
-  { 'goolord/alpha-nvim', enabled = true },
-  -- -- {
-  -- --   'lukas-reineke/indent-blankline.nvim',
-  -- --   opts = function(_, opts)
-  -- --     opts.filetype_exclude = vim.tbl_extend('force', opts.filetype_exclude, { 'nvdash', 'nvcheatsheet' })
-  -- --   end,
-  -- -- },
-  -- {
-  --   'echasnovski/mini.indentscope',
-  --   init = function()
-  --     vim.api.nvim_create_autocmd('FileType', {
-  --       pattern = { 'help', 'alpha', 'dashboard', 'neo-tree', 'Trouble', 'lazy', 'mason', 'nvdash', 'nvcheatsheet' },
-  --       callback = function()
-  --         vim.b.miniindentscope_disable = true
-  --       end,
-  --     })
-  --   end,
-  -- },
-  -- {
-  --   'kingavatar/nvchad-ui.nvim',
-  --   branch = 'v2.0',
-  --   lazy = false,
-  --   config = function()
-  --     require('nvchad_ui').setup {
-  --       lazyVim = true,
-  --       statusline = { separator_style = 'default', theme = 'default', lualine = true },
-  --       theme_toggle = { 'tokyonight', 'rose-pine' },
-  --       nvdash = { load_on_startup = true },
-  --     }
-  --
-  --     -- rename nvchad
-  --     vim.keymap.set('n', '<leader>cn', require('nvchad_ui.renamer').open, { desc = 'nvchad Rename' })
-  --   end,
-  -- },
+  { 'goolord/alpha-nvim' },
 }
